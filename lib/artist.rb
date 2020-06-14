41dfaf7e9229c738c813ae6e7d01c9545d6bb0b6
Elijah Kirschen <ekirschen584@gmail.com>
2020-06-14 01:31:33 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,45 @@
+require 'pry'
+
+class Artist
+    extend Concerns::Findable
+    attr_accessor :name
+    @@all =[]
+    
+    def initialize(name)
+    @name=name
+    @songs =[]
+    end
+    
+    def self.create(name)
+        artist = self.new(name)
+        artist.save
+        artist
+    end
+
+    def add_song(song)
+        song.artist= self unless song.artist == self
+        @songs << song unless self.songs.include?(song)
+    end
+
+    def genres
+        # binding.pry
+        @songs.map{|song| song.genre if song.artist == self}.uniq
+        
+    end
+    
+    def songs
+        @songs
+    end
+    
+    def self.all
+        @@all
+    end
+    
+    def save
+        @@all<<self
+    end
+    
+    def self.destroy_all
+    @@all.clear
+    end
+end
