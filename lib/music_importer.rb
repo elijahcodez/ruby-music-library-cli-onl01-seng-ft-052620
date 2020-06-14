ac130491026347c529786759133a7408ceba827e
Elijah Kirschen <ekirschen584@gmail.com>
2020-06-14 01:33:19 +0000

    Automatically backed up by Learn

---
+++ lib/music_importer.rb
@@ -0,0 +1,33 @@
+class MusicImporter
+    attr_accessor :path
+        @@all = []
+        def initialize(file_path)
+            @path =file_path
+            save
+        end
+    
+        def files
+            files = Dir.entries(@path)
+            mp3 = files.map{|file| file if file.match(/.mp3/)}.select{|k| k}
+            mp3
+        end
+    
+        def import
+            new_songs= files.map{|file| file.gsub('.mp3', '')}
+            new_songs.each{|song| Song.new_by_filename(song)}
+        end
+    
+        def import
+            files.each{|file| Song.create_from_filename(file)}
+            
+        end
+    
+        def save 
+            @@all << self
+        end
+    
+        def self.all
+            @@all
+        end
+    
+    end
